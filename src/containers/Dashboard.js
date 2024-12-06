e48a437a87920450bd987f8058d3bdb81f7c9104
Vanessa Peixoto <vanessa@mbp-de-vanessa.home>
2024-12-06 11:56:26 +0100

    bug dashboard admin fix

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -131,21 +131,22 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+
+    const arrowIcon = $(`#arrow-icon${index}`);
+    const statusContainer = $(`#status-bills-container${index}`);
+
+    const isExpanded = statusContainer.html().trim() !== "";
+
+    if(isExpanded) {
+      arrowIcon.css({ transform: 'rotate(90deg)'});
+      statusContainer.html("");
     } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
+      arrowIcon.css({ transform: 'rotate(0deg)'});
+      statusContainer.html(cards(filteredBills(bills, getStatus(index))))
     }
 
     bills.forEach(bill => {
+      $(`#open-bill${bill.id}`).off('click');
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
